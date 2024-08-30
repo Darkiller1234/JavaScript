e610aef4223267b4dc8e5618df8d960c53d955a7
Darkiler1234 <kyoung8292@naver.com>
2024-08-30 17:00:12 +0900

    0830

---
+++ resources/2.hosting.js
@@ -0,0 +1,23 @@
+/**
+ * Hoisting
+ */
+var name;
+let name2;
+
+
+console.log(name);
+var name = "daehan";
+console.log(name);
+
+/**
+ * Hoisting은 뭘까?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상'
+ * var와 다르게 let과 const는 tdz를 이용하여
+ * 마치 호이스팅이 일어나지 않은 것처럼 동작하게 해줌.
+ */
+
+
+console.log(name2);
+let name2 = "daehan";
+console.log(name2);
